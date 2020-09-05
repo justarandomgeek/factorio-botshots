c280ca3e91e0746b628136b735ff50ddb9024a9e
Thomas Deal <justarandomgeek@gmail.com>
2020-09-04 23:52:22 -0400

    spidershots

--- control.lua
+++ control.lua
@@ -18,23 +18,33 @@ end)
 script.on_event(defines.events.on_trigger_fired_artillery, function(event)
   local shell = event.entity
   local cannon = event.source
-  if shell.name == "botshots-projectile" and cannon.name == "botshots-turret" then
+  if not cannon then return end
+  if shell.name == "botshots-projectile" and cannon.name == "botshots-turret" or
+    shell.name == "spidershots-projectile" and cannon.name == "spidershots-turret" then
     -- disable this cannon until the shot hits, to keep it to one each.
     -- Without actually tracking the projectile itself there's no way to keep them in-order for concurrent shots to varied ranges
     cannon.active = false
 
     local chestinv = global.cannons[cannon.unit_number].chest.get_inventory(defines.inventory.chest)
+    local size = #chestinv
+    local inv = game.create_inventory(size)
+    for i = 1, size do
+      local slot = chestinv[i]
+      if slot.valid_for_read then
+        inv[i].set_stack(slot)
+      end
+    end
     global.shells[cannon.unit_number] = {
-      items = chestinv.get_contents()
+      inv = inv,
     }
     chestinv.clear()
-
   end
 end)
 
 script.on_event(defines.events.on_trigger_created_entity, function(event)
   local entity = event.entity
   local cannon = event.source
+  if not cannon then return end
   if entity.name == "botshots-roboport" and cannon.name == "botshots-turret" then
     -- grab the roboport, build the radar and chest, record them all along with tick to expire
     entity.minable=false
@@ -59,11 +69,20 @@ script.on_event(defines.events.on_trigger_created_entity, function(event)
     chest.minable=false
     chest.destructible = false
 
-    for name,count in pairs(global.shells[cannon.unit_number].items) do
-      if name == 'logistic-robot' or name == 'construction-robot' then
-        entity.insert{name=name,count=count}
-      else
-        chest.insert{name=name,count=count}
+    local shells = global.shells
+    local shell = shells[cannon.unit_number]
+    local inv = shell.inv
+    if inv and inv.valid then
+      for i = 1,#inv do
+        local slot = inv[i]
+        if slot.valid_for_read then
+          local name = slot.name
+          if name == 'logistic-robot' or name == 'construction-robot' then
+            entity.insert(slot)
+          else
+            chest.insert(slot)
+          end
+        end
       end
     end
 
@@ -77,46 +96,80 @@ script.on_event(defines.events.on_trigger_created_entity, function(event)
       chest=chest,
     }
 
-    global.outposts_expire[expire] = global.outposts_expire[expire] or {}
-    global.outposts_expire[expire][#global.outposts_expire[expire]+1] = entity.unit_number
+    local outposts_expire = global.outposts_expire
+    outposts_expire[expire] = outposts_expire[expire] or {}
+    outposts_expire[expire][#outposts_expire[expire]+1] = entity.unit_number
     global.outposts[entity.unit_number] = outpost
+  elseif entity.name == "spidertron" and cannon.name == "spidershots-turret" then
+    local shells = global.shells
+    local shell = shells[cannon.unit_number]
+    local inv = shell.inv
+    local grid = entity.grid
+    if inv and inv.valid then
+      for i = 1,#inv do
+        local slot = inv[i]
+        if slot.valid_for_read then
+          local equip = slot.prototype.place_as_equipment_result
+          if equip then
+            for j = 1,slot.count do
+              if not grid.put{name=equip.name} then
+                break
+              end
+              slot.count = slot.count - 1
+              if not slot.valid_for_read then
+                goto nextslot
+              end
+            end
+          end
+          entity.insert(slot)
+        end
+        ::nextslot::
+      end
+    end
+
+    shells[cannon.unit_number] = nil
+    cannon.active = true
   end
 end)
 
 script.on_event(defines.events.on_tick, function()
-  if global.outposts_expire[game.tick] then
-    for _,id in pairs(global.outposts_expire[game.tick]) do
+  local outposts_expire = global.outposts_expire
+  local outposts_expiring = outposts_expire[game.tick]
+  if outposts_expiring then
+    for _,id in pairs(outposts_expiring) do
 
       local outpost = global.outposts[id]
       if outpost then
-        global.outposts[outpost.roboport.unit_number] = nil
+        global.outposts[id] = nil
         --TODO: save the robots? launch them or transferto chest?
         -- morethan iniital bots may have parked here, if fired in range of an existing network for resupply
-        outpost.roboport.destroy()
-        outpost.radar.destroy()
+        if outpost.roboport.valid then outpost.roboport.destroy() end
+        if outpost.radar.valid then outpost.radar.destroy() end
 
-        outpost.chest.operable = true
-        outpost.chest.minable = true
-        outpost.chest.destructible = true
+        if outpost.chest.valid then
+          outpost.chest.operable = true
+          outpost.chest.minable = true
+          outpost.chest.destructible = true
+        end
       end
     end
     global.outposts_expire[game.tick] = nil
   end
 end)
 
-script.on_event({defines.events.on_robot_built_entity,defines.events.on_built_entity}, function(event)
-  local entity = event.created_entity
-  if entity.name == "botshots-turret" then
+local function onBuilt(entity)
+  if entity.name == "botshots-turret" or entity.name == "spidershots-turret" then
     local position = {entity.position.x,entity.position.y+1}
     local chest = entity.surface.find_entity('entity-ghost', position)
     if chest then
       _,chest = chest.revive()
     else
-      chest = entity.surface.create_entity{
-        name='botshots-chest',
-        position = position,
-        force = entity.force
-      }
+      chest = entity.surface.find_entity('botshots-chest', position) or
+        entity.surface.create_entity{
+          name='botshots-chest',
+          position = position,
+          force = entity.force
+        }
     end
 
     chest.minable=false
@@ -125,19 +178,26 @@ script.on_event({defines.events.on_robot_built_entity,defines.events.on_built_en
     global.cannons[entity.unit_number] = {
       cannon = entity,
       chest = chest,
-
     }
   end
-end)
+end
+
+script.on_event({defines.events.on_robot_built_entity,defines.events.on_built_entity}, function(event) onBuilt(event.created_entity) end)
+script.on_event(defines.events.on_entity_cloned, function(event) onBuilt(event.destination) end)
+script.on_event(defines.events.script_raised_built, function(event) onBuilt(event.entity) end)
+script.on_event(defines.events.script_raised_revive, function(event) onBuilt(event.entity) end)
+
 script.on_event({defines.events.on_robot_mined_entity,defines.events.on_player_mined_entity}, function(event)
   local entity = event.entity
-  if entity.name == "botshots-turret" then
-    for name,count in pairs(global.cannons[entity.unit_number].chest.get_inventory(defines.inventory.chest).get_contents()) do
-      event.buffer.insert{name=name,count=count}
+  if entity.name == "botshots-turret" or entity.name == "spidershots-turret" then
+    local chest = global.cannons[entity.unit_number].chest
+    if chest.valid then
+      for name,count in pairs(chest.get_inventory(defines.inventory.chest).get_contents()) do
+        event.buffer.insert{name=name,count=count}
+      end
+      chest.destroy()
     end
-    global.cannons[entity.unit_number].chest.destroy()
     global.cannons[entity.unit_number] = nil
-
   end
 end)
 script.on_event(defines.events.on_entity_died, function(event)
@@ -145,11 +205,12 @@ script.on_event(defines.events.on_entity_died, function(event)
   if entity.name == "botshots-roboport" then
     local outpost = global.outposts[entity.unit_number]
     global.outposts[entity.unit_number] = nil
-    outpost.radar.destroy()
-    outpost.chest.destroy()
+    if outpost.radar.valid then outpost.radar.destroy() end
+    if outpost.chest.valid then outpost.chest.destroy() end
 
-  elseif entity.name == "botshots-turret" then
-    global.cannons[entity.unit_number].chest.destroy()
+  elseif entity.name == "botshots-turret" or entity.name == "spidershots-turret" then
+    local chest = global.cannons[entity.unit_number].chest
+    if chest.valid then chest.destroy() end
     global.cannons[entity.unit_number] = nil
   end
 end)
--- data.lua
+++ data.lua
@@ -1,4 +1,4 @@
-function copyPrototype(type, name, newName)
+local function copyPrototype(type, name, newName)
   if not data.raw[type][name] then error("type "..type.." "..name.." doesn't exist") end
   local p = table.deepcopy(data.raw[type][name])
   p.name = newName
@@ -21,54 +21,106 @@ function copyPrototype(type, name, newName)
   return p
 end
 
+local function makeBotShots(prefix)
+  local botshots_gun = copyPrototype("gun","artillery-wagon-cannon",prefix.."-cannon")
+  botshots_gun.attack_parameters.ammo_category = prefix.."-shell"
 
-botshots_gun = copyPrototype("gun","artillery-wagon-cannon","botshots-cannon")
-botshots_gun.attack_parameters.ammo_category = "botshots-shell"
+  local botshots_turret = copyPrototype("artillery-turret","artillery-turret",prefix.."-turret")
+  botshots_turret.gun = botshots_gun.name
+  botshots_turret.disable_automatic_firing = true
+  botshots_turret.collision_box = {{-1.45, -1.45}, {1.45, 0.9}}
 
-botshots_turret = copyPrototype("artillery-turret","artillery-turret","botshots-turret")
-botshots_turret.gun = botshots_gun.name
-botshots_turret.disable_automatic_firing = true
-botshots_turret.collision_box = {{-1.45, -1.45}, {1.45, 0.9}}
+  local botshots_turret_item = copyPrototype("item","artillery-turret",prefix.."-turret")
 
-botshots_turret_item = copyPrototype("item","artillery-turret","botshots-turret")
+  local botshots_remote = copyPrototype("capsule","artillery-targeting-remote",prefix.."-targeting-remote")
+  botshots_remote.capsule_action.flare = prefix.."-flare"
 
-botshots_chest = copyPrototype("container","steel-chest","botshots-chest")
+  local botshots_flare = copyPrototype("artillery-flare","artillery-flare",prefix.."-flare")
+  botshots_flare.shot_category = prefix.."-shell"
+
+  local artytecheffects = data.raw["technology"]["artillery"].effects
+
+  artytecheffects[#artytecheffects+1] = { type = "unlock-recipe", recipe = prefix.."-cannon" }
+  artytecheffects[#artytecheffects+1] = { type = "unlock-recipe", recipe = prefix.."-shell" }
+  artytecheffects[#artytecheffects+1] = { type = "unlock-recipe", recipe = prefix.."-targeting-remote" }
+
+
+  data:extend({
+    {
+      type = "ammo-category",
+      name = prefix.."-shell"
+    },
+    botshots_gun,
+    botshots_turret,
+    botshots_turret_item,
+    botshots_remote,
+    botshots_flare,
+    {
+      type = "ammo",
+      name = prefix.."-shell",
+      icon = "__base__/graphics/icons/artillery-shell.png",
+      icon_size = 64, icon_mipmaps = 4,
+      ammo_type =
+      {
+        category = prefix.."-shell",
+        target_type = "position",
+        action =
+        {
+          type = "direct",
+          action_delivery =
+          {
+            type = "artillery",
+            projectile = prefix.."-projectile",
+            starting_speed = 1,
+            direction_deviation = 0,
+            range_deviation = 0,
+            trigger_fired_artillery = true,
+            source_effects =
+            {
+              type = "create-explosion",
+              entity_name = "artillery-cannon-muzzle-flash"
+            },
+          }
+        },
+      },
+      subgroup = "ammo",
+      order = "d[explosive-cannon-shell]-r[roboshell]",
+      stack_size = 1
+    },
+  }
+  )
+end
+
+
+-- and make the normal remote exclusive to normal shots
+data.raw["artillery-flare"]["artillery-flare"].shot_category = "artillery-shell"
+
+--TODO: rename things with bumble bots, use locale prefix "beeshots"
+makeBotShots("botshots")
+makeBotShots("spidershots")
+
+local botshots_chest = copyPrototype("container","steel-chest","botshots-chest")
 botshots_chest.collision_box = {{-1.45,  0.0}, {1.45, 0.4}}
 
-botshots_chest_item = copyPrototype("item","steel-chest","botshots-chest")
+local botshots_chest_item = copyPrototype("item","steel-chest","botshots-chest")
 
-
-botshots_roboport = copyPrototype("roboport","roboport","botshots-roboport")
+local botshots_roboport = copyPrototype("roboport","roboport","botshots-roboport")
 botshots_roboport.minable = nil
 botshots_roboport.order = "foo"
 botshots_roboport.energy_source = {type="void"}
 
-botshots_radar = copyPrototype("radar","radar","botshots-radar")
+local botshots_radar = copyPrototype("radar","radar","botshots-radar")
 botshots_radar.minable = nil
 botshots_radar.order = "foo"
 botshots_radar.energy_source = {type="void"}
 
-botshots_remote = copyPrototype("capsule","artillery-targeting-remote","botshots-targeting-remote")
-botshots_remote.capsule_action.flare = "botshots-flare"
 
-botshots_flare = copyPrototype("artillery-flare","artillery-flare","botshots-flare")
-botshots_flare.shot_category = "botshots-shell"
-
--- and make the normal remote exclusive to normla shots
-data.raw["artillery-flare"]["artillery-flare"].shot_category = "artillery-shell"
-
-local artytecheffects = data.raw["technology"]["artillery"].effects
-
-artytecheffects[#artytecheffects+1] = { type = "unlock-recipe", recipe = "botshots-cannon" }
-artytecheffects[#artytecheffects+1] = { type = "unlock-recipe", recipe = "botshots-shell" }
-artytecheffects[#artytecheffects+1] = { type = "unlock-recipe", recipe = "botshots-targeting-remote" }
-
-
-data:extend({
-  {
-    type = "ammo-category",
-    name = "botshots-shell"
-  },
+data:extend{
+  botshots_chest,
+  botshots_chest_item,
+  botshots_roboport,
+  botshots_radar,
+  -- botshots recipes
   {
     type = "recipe",
     name = "botshots-cannon",
@@ -107,47 +159,45 @@ data:extend({
     },
     result="botshots-targeting-remote",
   },
-  botshots_gun,
-  botshots_chest,
-  botshots_chest_item,
-  botshots_turret,
-  botshots_turret_item,
-  botshots_roboport,
-  botshots_radar,
-  botshots_remote,
-  botshots_flare,
+
+  -- spidershots recipes
   {
-    type = "ammo",
-    name = "botshots-shell",
-    icon = "__base__/graphics/icons/artillery-shell.png",
-    icon_size = 32,
-    ammo_type =
+    type = "recipe",
+    name = "spidershots-cannon",
+    enabled = "false",
+    ingredients =
     {
-      category = "botshots-shell",
-      target_type = "position",
-      action =
-      {
-        type = "direct",
-        action_delivery =
-        {
-          type = "artillery",
-          projectile = "botshots-projectile",
-          starting_speed = 1,
-          direction_deviation = 0,
-          range_deviation = 0,
-          trigger_fired_artillery = true,
-          source_effects =
-          {
-            type = "create-explosion",
-            entity_name = "artillery-cannon-muzzle-flash"
-          },
-        }
-      },
+      {"artillery-turret", 1},
+      {"spidertron-remote", 1},
     },
-    subgroup = "ammo",
-    order = "d[explosive-cannon-shell]-r[roboshell]",
-    stack_size = 1
+    energy_required = 10,
+    result="spidershots-turret",
   },
+  {
+    type = "recipe",
+    name = "spidershots-shell",
+    enabled = "false",
+    ingredients =
+    {
+      {"spidertron", 1},
+      {"cannon-shell", 4},
+    },
+    energy_required = 15,
+    result="spidershots-shell",
+  },
+  {
+    type = "recipe",
+    name = "spidershots-targeting-remote",
+    enabled = "false",
+    ingredients =
+    {
+      {"processing-unit", 1},
+      {"spidertron-remote", 1},
+    },
+    result="spidershots-targeting-remote",
+  },
+
+  -- projectiles
   {
     type = "artillery-projectile",
     name = "botshots-projectile",
@@ -247,5 +297,89 @@ data:extend({
     },
     height_from_ground = 280 / 64
   },
+  {
+    type = "artillery-projectile",
+    name = "spidershots-projectile",
+    flags = {"not-on-map"},
+    acceleration = 0,
+    direction_only = true,
+    reveal_map = true,
+    map_color = {r=0, g=0, b=1},
+    picture =
+    {
+      filename = "__base__/graphics/entity/artillery-projectile/hr-shell.png",
+      width = 64,
+      height = 64,
+      scale = 0.5,
+    },
+    shadow =
+    {
+      filename = "__base__/graphics/entity/artillery-projectile/hr-shell-shadow.png",
+      width = 64,
+      height = 64,
+      scale = 0.5,
+    },
+    chart_picture =
+    {
+      filename = "__base__/graphics/entity/artillery-projectile/artillery-shoot-map-visualization.png",
+      flags = { "icon" },
+      frame_count = 1,
+      width = 64,
+      height = 64,
+      priority = "high",
+      scale = 0.25,
+    },
+    action =
+    {
+      type = "direct",
+      action_delivery =
+      {
+        type = "instant",
+        target_effects =
+        {
+          {
+            type = "create-trivial-smoke",
+            smoke_name = "artillery-smoke",
+            initial_height = 0,
+            speed_from_center = 0.05,
+            speed_from_center_deviation = 0.005,
+            offset_deviation = {{-4, -4}, {4, 4}},
+            max_radius = 3.5,
+            repeat_count = 4 * 4 * 15
+          },
+          {
+            type = "create-entity",
+            entity_name = "spidertron",
+            check_buildability = false,
+            trigger_created_entity = true
+          },
+        }
+      }
+    },
+    final_action =
+    {
+      type = "direct",
+      action_delivery =
+      {
+        type = "instant",
+        target_effects =
+        {
+          {
+            type = "create-entity",
+            entity_name = "small-scorchmark",
+            check_buildability = true
+          }
+        }
+      }
+    },
+    animation =
+    {
+      filename = "__base__/graphics/entity/bullet/bullet.png",
+      frame_count = 1,
+      width = 3,
+      height = 50,
+      priority = "high"
+    },
+    height_from_ground = 280 / 64
+  },
 }
-)
